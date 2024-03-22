b6394d086a157b5c9208bc7ebc04d1d0e67780be
MarioParraL <mariovidovick@gmail.com>
2024-03-22 14:22:33 +0100

    changes

--- fresh-parcial/db/Contact.ts
+++ fresh-parcial/db/Contact.ts
@@ -2,11 +2,8 @@ import mongoose from "npm:mongoose";
 import { Contact } from "../types.ts";
 
 const MONGO_URL = Deno.env.get("MONGO_URL");
-if (!MONGO_URL) {
-  throw new Error("MONGO_URL not exist");
-}
 
-mongoose.connect(MONGO_URL);
+mongoose.connect("MONGO_URL");
 
 const contactSchema = new mongoose.Schema({
   name: String,
